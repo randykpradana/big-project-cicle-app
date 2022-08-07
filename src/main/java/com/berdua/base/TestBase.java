d39ee35d28e29435fa5646a43477a3b56c745d5d
Randy Pradana <randyk.pradana@gmail.com>
2022-08-08 02:36:16 +0700

    update object repository

--- src/main/java/com/berdua/base/TestBase.java
+++ src/main/java/com/berdua/base/TestBase.java
@@ -1,11 +1,14 @@
 package com.berdua.base;
 
+import com.berdua.utils.TestUtil;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class TestBase {
     public static WebDriver driver;
@@ -21,5 +24,20 @@ public class TestBase {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void initialization() {
+        String browserName = prop.getProperty("browser");
+        if (browserName.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+            driver = new ChromeDriver();
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);
+
+        driver.get(prop.getProperty("url"));
     }
 }
