2030c60c3d233411a80bfae87264e8ceb5c2db24
irfanplazuardi <irfanplazuardi@gmail.com>
2022-08-09 00:47:36 +0800

    update boardpage

--- src/main/java/com/berdua/pages/BoardPage.java
+++ src/main/java/com/berdua/pages/BoardPage.java
@@ -5,23 +5,76 @@ import org.openqa.selenium.support.FindBy;
 
 public class BoardPage {
 
-
+    // Page Factory
     @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div[1]/a/div")
-    WebElement BoardMenu;
+    WebElement boardMenu;
+
     @FindBy(xpath = "//*[@id=\"root\"]/div/div[3]/div/div/div[4]/button")
-    WebElement Addlist;
+    WebElement addList;
+
     @FindBy(xpath = "//*[@id=\"root\"]/div/div[3]/div/div/div[4]/div/div/form/div[1]/input")
-    WebElement ListName;
+    WebElement listName;
+
     @FindBy(xpath = "//*[@id=\"root\"]/div/div[3]/div/div/div[4]/div/div/form/div[2]/div/button")
-    WebElement CreateList;
+    WebElement createList;
+
     @FindBy(xpath = "//*[@id=\"root\"]/div/div[3]/div/div/div[4]/div/div/div[4]")
-    WebElement Addcard1;
-    @FindBy(xpath = "//*[@id=\"root\"]/div/div[3]/div/div/div[4]/div/div/div[4]/div[1]")
-    WebElement CardName;
+    WebElement addCard1;
+
     @FindBy(xpath = "//*[@id=\"root\"]/div/div[3]/div/div/div[4]/div/div/div[4]/div[3]/button")
-    WebElement Addcard2;
+    WebElement addCard2;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div[3]/div/div/div[4]/div/div/div[4]/div[1]")
+    WebElement cardName;
+
     @FindBy(xpath = "//*[@id=\"listcard_content-3\"]/div/div/div/a")
-    WebElement Cardclick;
+    WebElement cardBox;
+
     @FindBy(xpath = "//*[@id=\"cardModal\"]/div/div/div[2]/div[1]/div[1]/button")
-    WebElement Addmember;
+    WebElement addMember;
+    // Actions
+    public BoardPage clickBoardMenu() {
+        boardMenu.click();
+        return new BoardPage();
+    }
+
+    public BoardPage clickAddList() {
+        addList.click();
+        return new BoardPage();
+    }
+
+    public BoardPage clickListName() {
+        listName.click();
+        return new BoardPage();
+    }
+
+    public BoardPage clickCreateList() {
+        createList.sendKeys();
+        return new BoardPage();
+    }
+
+    public BoardPage clickAddCard1() {
+        addCard1.sendKeys();
+        return new BoardPage();
+    }
+
+    public BoardPage clickAddCard2() {
+        addCard2.sendKeys();
+        return new BoardPage();
+    }
+
+    public BoardPage clickCardName() {
+        cardName.sendKeys();
+        return new BoardPage();
+    }
+
+    public BoardPage clickCardBox() {
+        cardBox.sendKeys();
+        return new BoardPage();
+    }
+
+    public BoardPage clickAddMember() {
+        addMember.sendKeys();
+        return new BoardPage();
+    }
 }
