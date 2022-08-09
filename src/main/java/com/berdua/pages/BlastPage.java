1e4885edf801fbcaa80a405ac5639b7c618cb55e
Randy Pradana <randyk.pradana@gmail.com>
2022-08-09 22:51:56 +0700

    update blast test

--- src/main/java/com/berdua/pages/BlastPage.java
+++ src/main/java/com/berdua/pages/BlastPage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class BlastPage {
+    // Blasts List
     @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div[2]/div/div/div[1]/div[3]/div/div[2]/p")
     WebElement createBlastBarBtn;
     @FindBy(name = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div/div/div/div[1]/a/div")
@@ -14,6 +15,7 @@ public class BlastPage {
     WebElement editIcon;
     @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[1]/div[2]/div[2]/div/div/div[2]/div[1]/svg")
     WebElement archiveIcon;
+    // Blast Post Section
     @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[3]/div/div/div/div")
     WebElement cheersIcon;
     @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[3]/div/div/div/div/div/div[2]/form/input")
@@ -30,7 +32,61 @@ public class BlastPage {
     WebElement commentField;
     @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[4]/div/div/div/div[1]/div[2]/div[2]/div[2]/button[2]/div/h1")
     WebElement postButton;
-
+    @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div[1]/div[2]/div[1]/h1")
+    WebElement blastLabel;
+    @FindBy(xpath = "//*[@id=\"root\"]/div[2]/div[1]/div[2]/div[1]/h1")
+    WebElement createPostLabel;
+    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[1]/div")
+    WebElement postTitleField;
+    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[2]/div/div/div[3]/div")
+    WebElement postStoryField;
+    @FindBy(xpath = "//*[@id=\"bold-8\"]/svg")
+    WebElement boldIcon;
+    @FindBy(xpath = "//*[@id=\"italic-8\"]/svg")
+    WebElement italicIcon;
+    @FindBy(xpath = "//*[@id=\"strikeThrough-8\"]/svg")
+    WebElement strikethroughIcon;
+    @FindBy(xpath = "//*[@id=\"backgroundColor-8\"]/svg")
+    WebElement bgColorIcon;
+    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[2]/div/div/div[1]/div[10]/div[1]/span[1]")
+    WebElement greenBgColor;
+    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[2]/div/div/div[1]/div[10]/div[1]/span[28]")
+    WebElement trashBgColor;
+    @FindBy(xpath = "//*[@id=\"fontSize-8\"]/svg")
+    WebElement fontSizeDrpDwn;
+    @FindBy(xpath = "//*[@id=\"dropdown-menu-fontSize-8\"]/div/div/ul/li[11]/a")
+    WebElement fontSizePts;
+    @FindBy(xpath = "//*[@id=\"moreText-8\"]/svg")
+    WebElement moreTextIcon;
+    @FindBy(xpath = "//*[@id=\"underline-8\"]/svg")
+    WebElement underlineIcon;
+    @FindBy(xpath = "//*[@id=\"subscript-8\"]")
+    WebElement subscriptIcon;
+    @FindBy(xpath = "//*[@id=\"superscript-8\"]/svg")
+    WebElement superscriptIcon;
+    @FindBy(xpath = "//*[@id=\"underline-8\"]/svg")
+    WebElement fontFamilyDrpDwn;
+    @FindBy(xpath = "//*[@id=\"dropdown-menu-fontFamily-8\"]/div/div/ul/li[5]/a")
+    WebElement tnrFontStyle;
+    @FindBy(xpath = "//*[@id=\"textColor-8\"]/svg")
+    WebElement textColorIcon;
+    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[2]/div/div/div[1]/div[11]/div[1]/span[4]")
+    WebElement blueTextColor;
+    @FindBy(xpath = "//*[@id=\"root\"]/div[3]/div[2]/div/div/div[2]/div/div/div[1]/div[9]/div[1]/span[28]/svg")
+    WebElement trashTextColor;
+    @FindBy(xpath = "//*[@id=\"clearFormatting-1\"]/svg")
+    WebElement clearFormatting;
+    @FindBy(xpath = "//*[@id=\"formatOLSimple-1\"]/svg")
+    WebElement orderedListIcon;
+    @FindBy(xpath = "//*[@id=\"formatUL-1\"]/svg")
+    WebElement unorderedListIcon;
+    @FindBy(xpath = "//*[@id=\"formatULOptions-1\"]")
+    WebElement unorderedListDrpDwn;
+    @FindBy(xpath = "//*[@id=\"moreParagraph-1\"]/svg")
+    WebElement moreParagraphIcon;
+    @FindBy(xpath = "//*[@id=\"alignCenter-1\"]/svg")
+    WebElement alignCenter;
+    // Blasts List Methods
     public BlastPage clickCreateBlastBarBtn() {
         createBlastBarBtn.click();
         return new BlastPage();
@@ -51,6 +107,7 @@ public class BlastPage {
         archiveIcon.click();
         return new BlastPage();
     }
+    // Blast Post Section Methods
     public BlastPage clickCheersIcon() {
         cheersIcon.click();
         return new BlastPage();
@@ -81,6 +138,14 @@ public class BlastPage {
     }
     public BlastPage clickPostButton() {
         postButton.click();
+        return new BlastPage();
+    }
+    public BlastPage displayBlastLabel() {
+        blastLabel.isDisplayed();
+        return new BlastPage();
+    }
+    public BlastPage displayCreatePostLabel() {
+        blastLabel.isDisplayed();
         return new BlastPage();
     }
 }
---
+++ src/test/java/com/berdua/test/BlastTest.java
@@ -0,0 +1,83 @@
+package com.berdua.test;
+
+import com.berdua.base.TestBase;
+import com.berdua.pages.BlastPage;
+import com.berdua.pages.HomePage;
+import com.berdua.pages.LoginPage;
+import com.berdua.pages.TeamPage;
+import org.junit.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class BlastTest extends TestBase {
+    LoginPage loginPage;
+    HomePage homePage;
+    TeamPage teamPage;
+    BlastPage blastPage;
+
+    public BlastTest() {
+        super();
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        initialization();
+        loginPage = new LoginPage();
+    }
+    @Test(priority=1)
+    public void B01_User_directed_to_Blast_page_when_select_Blast_on_dropdown_navigation_bar() {
+        loginPage.clickLoginButton();
+        loginPage.enterEmailField(prop.getProperty("username"));
+        loginPage.clickLoginNextButton();
+        loginPage.enterPasswordField(prop.getProperty("password"));
+        loginPage.clickPassNextButton();
+        loginPage.selectCompanyLabel();
+
+        homePage.clickNavbarDrpDwnIcon();
+        homePage.selectBlastDrpDwn();
+
+        blastPage.displayBlastLabel();
+        Assert.assertEquals("Blast", "Blast");
+    }
+    @Test
+    public void B02_User_directed_to_Create_a_Blast_page_when_click_Create_Post_bar_button() {
+        loginPage.clickLoginButton();
+        loginPage.enterEmailField(prop.getProperty("username"));
+        loginPage.clickLoginNextButton();
+        loginPage.enterPasswordField(prop.getProperty("password"));
+        loginPage.clickPassNextButton();
+        loginPage.selectCompanyLabel();
+
+        homePage.clickNavbarDrpDwnIcon();
+        homePage.selectBlastDrpDwn();
+
+        teamPage.clickBlastWidget();
+
+        blastPage.clickCreateBlastBarBtn();
+        blastPage.displayCreatePostLabel();
+        Assert.assertEquals("Create a Blast", "Create a Blast");
+    }
+    @Test
+    public void User_create_a_new_post_with_all_completed_fields() {
+        loginPage.clickLoginButton();
+        loginPage.enterEmailField(prop.getProperty("username"));
+        loginPage.clickLoginNextButton();
+        loginPage.enterPasswordField(prop.getProperty("password"));
+        loginPage.clickPassNextButton();
+        loginPage.selectCompanyLabel();
+
+        homePage.clickNavbarDrpDwnIcon();
+        homePage.selectBlastDrpDwn();
+
+        teamPage.clickBlastWidget();
+
+        blastPage.clickCreateBlastBarBtn();
+        blastPage.displayCreatePostLabel();
+        Assert.assertEquals("Create a Blast", "Create a Blast");
+    }
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
