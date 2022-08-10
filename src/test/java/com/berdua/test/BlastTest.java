3e088fb127a9c8cbb7f754519a0da397690e12a4
Fathur Rohim <trustoim@gmail.com>
2022-08-10 21:47:47 +0700

    Update: Refactor Helper to manual login by test priority

--- src/test/java/com/berdua/test/BlastTest.java
+++ src/test/java/com/berdua/test/BlastTest.java
@@ -1,7 +1,6 @@
 package com.berdua.test;
 
 import com.berdua.base.TestBase;
-import com.berdua.helper.Login;
 import com.berdua.pages.BlastPage;
 import com.berdua.pages.HomePage;
 import com.berdua.pages.LoginPage;
@@ -12,13 +11,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BlastTest extends TestBase {
-    /**
-     * Helper Start
-     */
-    Login loginHelper;
-    /**
-     * Helper End
-     */
+
     LoginPage loginPage;
     HomePage homePage;
     TeamPage teamPage;
@@ -32,7 +25,13 @@ public class BlastTest extends TestBase {
     public void setUp() {
         initialization();
         loginPage = new LoginPage();
-        homePage = loginHelper.login(prop.getProperty("username"), prop.getProperty("password"));
+        loginPage.clickLoginButton();
+        loginPage.enterEmailField(prop.getProperty("username"));
+        loginPage.clickLoginNextButton();
+        loginPage.enterPasswordField(prop.getProperty("password"));
+        loginPage.clickPassNextButton();
+        loginPage.selectCompanyLabel();
+        org.testng.Assert.assertEquals(loginPage.validateDashboard(), "Home");
     }
     @Test(priority = 1)
     public void B01_User_directed_to_Blast_page_when_select_Blast_on_dropdown_navigation_bar() {
